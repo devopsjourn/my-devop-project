9f419c971d10135a7b1893b2799ef8896eafedfe
OpeyemiAjibola <opeyemiajibola@gmai.com>
2022-09-08 05:29:37 +0000

    my first gcp terraform commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
